732498097632043d5d84e025ec9892a3074d7563
fraser jamieson <fraserjamieson.93@gmail.com>
2021-02-07 17:25:49 +0000

    1st shop test

--- src/test/java/MusicStoreTest.java
+++ src/test/java/MusicStoreTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MusicStoreTest {
+
+    MusicStore shop;
+
+    @Before
+    public void setUp(){
+        shop = new MusicStore();
+    }
+
+    @Test
+    public void hasEmptyStockList(){
+        assertEquals(0, shop.getStock().size());
+    }
+
+
 }
